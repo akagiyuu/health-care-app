83eb59918c82c429f25f44ff854e518d47eb89e5
AkagiYuu <khanghuynh0245@gmail.com>
2022-11-16 23:59:18 +0700

    Update

--- src/features/health/components/InfoGraph.tsx
+++ src/features/health/components/InfoGraph.tsx
@@ -4,31 +4,56 @@ import { Button, View } from 'react-native';
 import Firebase from '../../../services/Firebase';
 import { HealthKey } from '../types';
 
-export const InfoGraph: FC<
-    PropsWithChildren<{ label: HealthKey; latest_value: number }>
-> = ({ label, latest_value }) => {
-    const [record, set_record] = useState([
-        { time: 0, value: 0 }
-    ]);
-    const [maxYear, setMaxYear] = useState(0);
-    console.log("Record: ", record);
+const take_last = (
+    array: { time: number; value: number }[],
+    number_of_elements: number,
+) => {
+    let length = array.length;
+    if (length <= number_of_elements) return array;
+    let result = Array(number_of_elements);
+    for (let i = 0; i < number_of_elements; i++) {
+        console.log(array[length - 1 - i]);
+        result[i] = {
+            time: i,
+            value: array[length - 1 - number_of_elements + i].value,
+        };
+    }
+
+    return result;
+};
+
+export const InfoGraph = ({
+    label,
+    latest_value,
+    number_of_records,
+    domain,
+}: {
+    label: HealthKey;
+    latest_value: number;
+    number_of_records: number;
+    domain?: any;
+}) => {
+    const [record, set_record] = useState([{ time: 0, value: 0 }]);
+    console.log('Label: ', label);
+    console.log('Record: ', record);
 
     useEffect(() => {
         const append_chart = (latest_value: number) => {
             console.log(latest_value);
-            var data = record.slice(Math.max(record.length - 4, 0))
+            var data = take_last(record, number_of_records - 1);
             var obj = {
-                time: `${maxYear}`,
+                time: `${data.length}`,
                 value: latest_value,
             };
             data.push(obj);
             set_record(data);
-            setMaxYear(maxYear + 1);
         };
         append_chart(latest_value);
     }, [latest_value]);
     return (
         <VictoryLine
+            domain={domain}
+            standalone={false}
             animate={{
                 duration: 2000,
                 onLoad: { duration: 1000 },
--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
-import { VictoryChart } from 'victory-native';
+import { VictoryChart, VictoryGroup } from 'victory-native';
 
 import { HealthLimit } from '../data/HealthLimit';
 import { Info } from '../features/health';
 import { InfoGraph } from '../features/health/components/InfoGraph';
 import { Default } from '../features/health/types';
 import Firebase from '../services/Firebase';
+
+const NUMBER_OF_RECORD = 3;
+const MAX_VALUE = 300;
 
 const HealthPage = () => {
     const [health, set_health] = useState(Default);
@@ -16,9 +19,22 @@ const HealthPage = () => {
     }, []);
 
     return (
-        <VictoryChart width={350}>
-            <InfoGraph label="Heart rate" latest_value={health['Heart rate']} />
-            <InfoGraph label="SP O2" latest_value={health['SP O2']} />
+        <VictoryChart
+            domain={{ x: [0, NUMBER_OF_RECORD - 1], y: [0, MAX_VALUE] }}>
+            <VictoryGroup>
+                <InfoGraph
+                    label="Heart rate"
+                    latest_value={health['Heart rate']}
+                    number_of_records={NUMBER_OF_RECORD}
+                />
+            </VictoryGroup>
+            <VictoryGroup>
+                <InfoGraph
+                    label="SP O2"
+                    latest_value={health['SP O2']}
+                    number_of_records={NUMBER_OF_RECORD}
+                />
+            </VictoryGroup>
         </VictoryChart>
     );
 };
